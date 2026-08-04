3eb2ef31bef9b6380f698256db1b377ad5d041f5
Vaishali <vaishalikolpe40@gmail.com>
2026-08-04 13:18:14 +0530

    Add Footer section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Features />
       <HowItWorks />
+      <Footer />
     </div>
   )
 }
